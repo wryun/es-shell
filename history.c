36d18713a4c1ccce313c96e1ce289c16ab6b0f3b
jpco <jack.p.conger@gmail.com>
2023-11-27 19:20:26 -0800

    Use readline history functions when using readline.
    This allows, for example, the use of `history_write_timestamps`, which
    inserts a `#` and a timestamp before each command written to history.
    This then allows multi-line commands to be properly read from the
    history file.
    
    That feature is left disabled, though, because it writes incompatible
    history files.

--- history.c
+++ history.c
@@ -16,14 +16,25 @@
  * globals
  */
 
-static char *history;
-static int historyfd = -1;
 static Buffer *histbuffer = NULL;
+static char *history;
 
 #if READLINE
 extern void add_history(char *);
 extern int read_history(char *);
 extern void stifle_history(int);
+extern int append_history(int, const char *);
+extern void using_history(void);
+
+#if 0
+/* These split history file entries by timestamp, which allows readline to pick up
+ * multi-line commands correctly across process boundaries.  Disabled by default,
+ * because it leaves the history file itself kind of ugly. */
+static int history_write_timestamps = 1;
+static char history_comment_char = '#';
+#endif
+#else
+static int historyfd = -1;
 #endif
 
 
@@ -80,15 +91,31 @@ extern void discardhistbuf() {
  * history file
  */
 
-/* loghistory -- write the last command out to a file */
+#if READLINE
 extern void loghistory(char *cmd) {
-	size_t len;
+	int err;
 	if (cmd == NULL)
 		return;
-#if READLINE
 	add_history(cmd);
-#endif
 	if (history == NULL)
+		return;
+
+	if ((err = append_history(1, history))) {
+		eprint("history(%s): %s\n", history, esstrerror(err));
+		vardef("history", NULL, NULL);
+	}
+}
+
+extern void sethistory(char *file) {
+	/* Attempt to populate readline history with new history file. */
+	stifle_history(50000); /* Keep memory usage within sane-ish bounds. */
+	read_history(file);
+	history = file;
+}
+#else /* !READLINE */
+extern void loghistory(char *cmd) {
+	size_t len;
+	if (cmd == NULL || history == NULL)
 		return;
 	if (historyfd == -1) {
 		historyfd = eopen(history, oAppend);
@@ -103,19 +130,14 @@ extern void loghistory(char *cmd) {
 	ewrite(historyfd, "\n", 1);
 }
 
-/* sethistory -- change the file for the history log */
 extern void sethistory(char *file) {
 	if (historyfd != -1) {
 		close(historyfd);
 		historyfd = -1;
 	}
-#if READLINE
-	/* Attempt to populate readline history with new history file. */
-	stifle_history(50000); /* Keep memory usage within sane-ish bounds. */
-	read_history(file);
-#endif
 	history = file;
 }
+#endif
 
 
 /*
@@ -127,6 +149,10 @@ extern void inithistory(void) {
 	/* declare the global roots */
 	globalroot(&history);		/* history file */
 
+#if READLINE
+	using_history();
+#else
 	/* mark the historyfd as a file descriptor to hold back from forked children */
 	registerfd(&historyfd, TRUE);
+#endif
 }
